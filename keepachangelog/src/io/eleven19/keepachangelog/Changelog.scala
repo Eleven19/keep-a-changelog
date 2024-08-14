b3742f4f88b8021b15a56594b6395f943120a3c2
Damian Reeves <957246+DamianReeves@users.noreply.github.com>
2024-08-14 15:38:04 -0500

    Working on Changelog population

--- keepachangelog/src/io/eleven19/keepachangelog/Changelog.scala
+++ keepachangelog/src/io/eleven19/keepachangelog/Changelog.scala
@@ -3,48 +3,125 @@ import errors.ParsingError
 import laika.api._
 import laika.ast._
 import laika.format.Markdown
+import java.time.LocalDate
+import just.semver.SemVer
+import cats.data.Op
+
+sealed abstract class ChangelogElement extends Product with Serializable
 
 final case class Changelog(
     projectName: Option[String],
     description: Option[String],
     entries: IndexedSeq[ChangelogEntry]
-) {
+) extends ChangelogElement {
   self =>
-
-  def projectNameOrDefault(default: String): String = projectName.getOrElse(default)
-  def descriptionOrDefault(default: String): String = description.getOrElse(default)
-  def withSortedEntries: Changelog                  = self.copy(entries = entries.sorted)
+  def +(entry: ChangelogEntry): Changelog                         = self.copy(entries = entries.appended(entry))
+  def projectNameOrDefault(default: String): String               = projectName.getOrElse(default)
+  def descriptionOrDefault(default: String): String               = description.getOrElse(default)
+  def withEntries(entries: IndexedSeq[ChangelogEntry]): Changelog = self.copy(entries = entries)
+  def withEntries(entries: ChangelogEntry*): Changelog            = self.copy(entries = entries.toIndexedSeq)
+  def withSortedEntries: Changelog                                = self.copy(entries = entries.sorted)
 }
 
 object Changelog {
   val empty: Changelog = Changelog(None, None, IndexedSeq())
 
-  def fromDocument(document: Document): Either[ParsingError, Changelog] = {
-    var first                       = true
-    var projectName: Option[String] = None
-    val description: Option[String] = None
-    document.sections.foreach { section =>
-      if (first) {
-        projectName =
-          Option(section.title.content.collect { case container: TextContainer => container.content }.mkString)
-        println(s"[First Section Title]${section.title}")
-        first = false
-      }
+  def apply(projectName: String): Changelog         = Changelog(Some(projectName), None, IndexedSeq())
+  def apply(projectName: Option[String]): Changelog = Changelog(projectName, None, IndexedSeq())
+  def apply(projectName: String, description: String): Changelog =
+    Changelog(Some(projectName), Some(description), IndexedSeq())
+
+  object HeaderText {
+    def unapply(input: Any): Option[String] = input match {
+      case element: ElementTraversal =>
+        val spans = extractTextSpans(element)
+        val text  = spans.map(_.content).mkString
+        Option(text)
+      case _ => None
     }
-    Right(
-      Changelog(
-        projectName = projectName,
-        description = description,
-        entries = IndexedSeq()
-      )
+  }
+
+  private def extractTextSpans(element: ElementTraversal): List[Text] =
+    element.collect {
+      case (h1 @ Header(1, _, _)) => h1.collect { case span: Text => span }
+    }.flatten
+
+  private[keepachangelog] def fromSection(section: Section) = {
+    val projectName = section.header.extractText
+    val (paragraphs, _) = {
+      val paras = List.newBuilder[Paragraph]
+      val secs  = List.newBuilder[Section]
+      section.content.foreach {
+        case p: Paragraph => paras += p
+        case s: Section   => secs += s
+        case _            =>
+      }
+      (paras.result(), secs.result())
+    }
+    val description = paragraphs.map(_.extractText).mkString(System.lineSeparator())
+    Changelog(
+      projectName = projectName,
+      description = description
     )
   }
+
+  def fromDocument(document: Document): Changelog =
+    document.content.collect {
+      case (section @ Section(Header(1, _, _), _, _)) => Changelog.fromSection(section)
+    }.take(1).head
 
   def fromMarkdownText(markdownText: String): Either[ParsingError, Changelog] = {
     val parserBuilder = MarkupParser.of(Markdown)
     val parser        = parserBuilder.build
 
     val result = parser.parse(markdownText).left.map(e => ParsingError.ChangelogDocumentParseError(e.message))
-    result.flatMap(fromDocument)
+    result.map(fromDocument)
   }
+}
+
+sealed trait ChangelogEntry extends ChangelogElement with Ordered[ChangelogEntry] { self =>
+
+  def compare(that: ChangelogEntry): Int =
+    (self, that) match {
+      case (ChangelogEntry.Unreleased(_), ChangelogEntry.Unreleased(_)) =>
+        0
+      case _ => ???
+    }
+
+  def wasYanked: Boolean = self match {
+    case ChangelogEntry.Released(_, _, yanked) => yanked
+    case _                                     => false
+  }
+
+  def releaseDate: Option[LocalDate] = self match {
+    case ChangelogEntry.Released(_, date, _) => Some(date)
+    case _                                   => None
+  }
+}
+
+sealed trait VersionedChangelogEntry extends ChangelogEntry {
+  def version: SemVer
+}
+
+object ChangelogEntry {
+
+  object Version {
+    def unapply(input: String): Option[SemVer] = SemVer.parse(input).toOption
+  }
+
+  final case class Released(version: SemVer, date: LocalDate, yanked: Boolean) extends VersionedChangelogEntry
+  final case class Versioned(version: SemVer)                                  extends VersionedChangelogEntry
+  final case class Unreleased(contents: IndexedSeq[ChangelogSection])          extends ChangelogEntry
+
+}
+
+final case class ChangelogSection(changeType: ChangeType, items: IndexedSeq[String]) extends ChangelogElement { self =>
+  def isCustomChangeType: Boolean = changeType match {
+    case ChangeType.Custom(_) => true
+    case _                    => false
+  }
+}
+
+object ChangelogSection {
+  implicit val ordering: Ordering[ChangelogSection] = Ordering.by((section: ChangelogSection) => section.changeType)
 }
--- keepachangelog/test/src/io/eleven19/keepachangelog/ChangelogSpec.scala
+++ keepachangelog/test/src/io/eleven19/keepachangelog/ChangelogSpec.scala
@@ -1,6 +1,9 @@
 package io.eleven19.keepachangelog
 import zio._
 import zio.test._
+import laika.api._
+import laika.ast._
+import laika.format._
 
 object ChangelogSpec extends ZIOSpecDefault {
 
@@ -13,18 +16,81 @@ object ChangelogSpec extends ZIOSpecDefault {
               Live.live(
                 ChangelogSampleDataProvider.standardSampleContent
               ) // NOTE: In order to get the real environment variables we need to use Live.live
-            changelog <- ZIO.fromEither(Changelog.fromMarkdownText(changelogContents))
+            changelog <- ZIO.fromEither(Changelog.fromMarkdownText(changelogContents))            
+            _         <- ZIO.logInfo(s"[Changelog]: $changelog")            
           } yield Chunk(
             test("should have the correct projectName") {
               assertTrue(changelog.projectName == Some("Changelog"))
             },
             test("should have the correct description") {
               assertTrue(
-                changelog.description == Some("All notable changes to this project will be documented in this file.")
+                changelog.description.get.startsWith(
+                  "All notable changes to this project will be documented in this file."
+                )
               )
-            }
+            },
+            suite("Exploring Markdown parsing")(
+              test("Document transforms") {
+                val parserBuilder = MarkupParser.of(Markdown)
+                val parser        = parserBuilder.build
+                val result        = parser.parse(changelogContents)
+
+                result.fold(
+                  e => println(s"Failed to parse markdown: $e"),
+                  document => transformDocument(document)
+                )
+                assertTrue(result.isRight)
+              }
+            )
           )
         }
       )
     ).provideShared(ChangelogSampleDataProvider.live)
+
+  def transformDocument(document: Document) = {
+    val stack     = scala.collection.mutable.Stack.empty[ChangelogElement]
+    var changelog = Changelog.empty
+    val headers = document.content.collect {
+      case (h1 @ Header(1, _, _)) =>
+        val projectName = Option(h1.extractText)
+        val description = Option(h1.collect {
+          case container: TextContainer => container.content
+        }.mkString)
+        changelog = changelog.copy(projectName = projectName, description = description)
+        stack.push(changelog)
+        h1
+      case (h2 @ Header(2, _, _)) =>
+        val textNodes = h2.collect {
+          case container: TextContainer => container.content
+        }
+        val maybeEntry = textNodes match {
+          case "Unreleased" :: Nil                 => Some(ChangelogEntry.Unreleased(IndexedSeq.empty))
+          case ChangelogEntry.Version(semVer) :: _ => Some(ChangelogEntry.Versioned(semVer))
+          case _                                   => None
+        }
+        maybeEntry.foreach { entry =>
+          stack.push(entry)
+          changelog = changelog + entry
+        }
+        h2
+    }
+
+    val sections = document.content.collect {
+      case (section @ Section(header @ Header(1, _, _), contents, _)) =>
+        val items =
+          for {
+            content <- contents.map {
+              case Section(h, _, _)            => s"Section: ${h.extractText}"
+              case (p @ Paragraph(content, _)) => s"P: ${p.extractText}"
+              case _                           => "Something else"
+            }
+          } yield content
+        (header.level, header.extractText, items)
+    }
+    // println(s"headers: $headers")
+    println("=================================================")
+    println(s"changelog: $changelog")
+    println("=================================================")
+    println(s"sections: $sections")
+  }
 }
